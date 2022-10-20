a3344b7d5b5430ee75573ed5047e66e0f9ca933b
Aamir <113689022+aliamirr@users.noreply.github.com>
2022-10-20 10:57:11 +0530

    Update Fibonacci.java

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -23,3 +23,27 @@ public class Fibonacci {
         }
     }
 }
+
+
+//Fibonacci number at exact index required.
+
+// public class Fibonacci {
+//     public static void main(String args[]) {
+//     System.out.println("Enter the index of which Fibonacci number is required ");
+//         Scanner input = new Scanner(System.in);
+//         int n = in.nextInt();
+//         int a = 0;
+//         int b = 1;
+//         int count = 2;
+        
+//         while (count <= n) {
+//             int temp = b;
+//             b = b + a;
+//             a = temp;
+//             count++;
+//         }    
+        
+//         System.out.println(b);
+        
+//     }  
+// }  
